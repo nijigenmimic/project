bb02819230db9ab22c84365308b0b615fb3ad48b
nijigenmimic <nijigenmimic@gmail.com>
2024-04-13 16:55:38 +0900

    New

---
+++ src/app/app.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let controller: AppController;
+  let service: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    controller = app.get<AppController>(AppController);
+    service = app.get<AppService>(AppService);
+  });
+
+  describe('/get init', () => {
+  });
+});
